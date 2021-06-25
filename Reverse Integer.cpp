d24b5a3e018deee7d18b9eb46c84ed512b6899c0
AviralSrivastava-star <76515297+AviralSrivastava-star@users.noreply.github.com>
2021-06-26 00:38:25 +0530

    Update Reverse Integer.cpp

--- Reverse Integer.cpp
+++ Reverse Integer.cpp
@@ -35,11 +35,11 @@ public:
         
         int rev = 0;
         while (x != 0) {
-            int pop = x % 10;
+            int rem = x % 10;
             x /= 10;
-            if (rev > INT_MAX/10 || (rev == INT_MAX / 10 && pop > 7)) return 0;
-            if (rev < INT_MIN/10 || (rev == INT_MIN / 10 && pop < -8)) return 0;
-            rev = rev * 10 + pop;
+            if (rev > INT_MAX/10 || (rev == INT_MAX / 10 && rem > 7)) return 0;
+            if (rev < INT_MIN/10 || (rev == INT_MIN / 10 && rem < -8)) return 0;
+            rev = rev * 10 + rem;
         }
         return rev;
     }
