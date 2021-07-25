64269df578c644ce79ed787ac345e0eba8ea2c92
AviralSrivastava-star <76515297+AviralSrivastava-star@users.noreply.github.com>
2021-07-25 22:49:02 +0530

    Create 5.Count Primes.cpp

---
+++ 5.Count Primes.cpp
@@ -0,0 +1,47 @@
+Count the number of prime numbers less than a non-negative number, n.
+
+ 
+
+Example 1:
+
+Input: n = 10
+Output: 4
+Explanation: There are 4 prime numbers less than 10, they are 2, 3, 5, 7.
+Example 2:
+
+Input: n = 0
+Output: 0
+Example 3:
+
+Input: n = 1
+Output: 0
+ 
+
+Constraints:
+
+0 <= n <= 5 * 106
+  
+  
+  
+  
+  
+  
+  class Solution {
+public:
+    int countPrimes(int n) {
+        int result = 0;
+        vector<int> vec(n,0);
+        for(int i=2; i<n; i++){
+            if(!vec[i]){
+                int temp = i;
+                while(temp+i<n){
+                    temp += i;
+                    vec[temp] = 1;
+                }
+                result += 1;
+            }
+        }
+        return result;
+    
+    }
+};
